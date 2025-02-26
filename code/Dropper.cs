fe7b7ad1f4cf05314af1df87d236e202f851e252
Tanner <tjlang1120@gmail.com>
2025-02-25 20:47:52 -0500

    SpawnProduct
    Programmed the dropper component to create a the Dropped product. Attached the Product component to the Dropped_Product to give it more functionality down the line.

--- code/Dropper.cs
+++ code/Dropper.cs
@@ -2,6 +2,10 @@ using Sandbox;
 
 public sealed class Dropper : Component
 {
+	[Property] public float DropRate { get; set; } = 2.0f;
+
+
+	private TimeSince lastDrop = 0;
 
 	protected override void OnAwake()
 	{
@@ -9,8 +13,38 @@ public sealed class Dropper : Component
 	}
 	protected override void OnUpdate()
 	{
-		//spawn items
+		if ( lastDrop > +DropRate )
+		{
+			spawnProduct();
+			lastDrop = 0;
+		}
 	}
 
+	private void spawnProduct()
+	{
+		//Set parameters of the object
+		Vector3 spawnPosition = GameObject.WorldPosition + Vector3.Up * 50 + Vector3.Forward * 75;
+		GameObject product = new GameObject();
+		product.WorldPosition = spawnPosition;
+		product.WorldScale = new Vector3( 0.1f, 0.1f, 0.1f );
+		product.Name = "Dropped Product";
+
+		//creating the model
+		var model = product.Components.Create<ModelRenderer>();
+		model.Model = Model.Load( "models/dev/box.vmdl" );
+
+		//create collider, necessary to add physics 
+		var collider = product.Components.Create<BoxCollider>();
+
+		//create rigid body for gravity and interactions with other objects physics.
+		var rigidBody = product.Components.Create<Rigidbody>();
+		rigidBody.MotionEnabled = true;
+		rigidBody.Gravity = true;
+
+		//attach product component
+		product.Components.Create<Product>();
+
+		Log.Info( "Product Dropped at {spawnPosition}" );
+	}
 
 }
--- code/Product.cs
+++ code/Product.cs
@@ -2,7 +2,12 @@
 
 public sealed class Product : Component
 {
+	[Property] public int Value { get; set; } = 100;
 
+	protected override void OnStart()
+	{
+		Log.Info( $"Prodcut Dropped with value: {Value}" );
+	}
 	protected override void OnAwake()
 	{
 
